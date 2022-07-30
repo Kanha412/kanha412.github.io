105f2292c0f8c521e55140583f54da832bded946
KANHA GUPTA <kanhagupta412@gmail.com>
2022-07-30 15:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,32 @@ span.onclick = function() {
 
 
 // Get the modal
-var modal = document.getElementById("myModal1");
+var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg2");
 var modalImg = document.getElementById("img011");
 var captionText = document.getElementById("caption1");
 img.onclick = function(){
@@ -68,7 +90,7 @@ span.onclick = function() {
 var modal = document.getElementById("myModal2");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg2");
+var img = document.getElementById("myImg3");
 var modalImg = document.getElementById("img012");
 var captionText = document.getElementById("caption2");
 img.onclick = function(){
@@ -90,7 +112,7 @@ span.onclick = function() {
 var modal = document.getElementById("myModal3");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg3");
+var img = document.getElementById("myImg4");
 var modalImg = document.getElementById("img013");
 var captionText = document.getElementById("caption3");
 img.onclick = function(){
@@ -112,7 +134,7 @@ span.onclick = function() {
 var modal = document.getElementById("myModal4");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg4");
+var img = document.getElementById("myImg5");
 var modalImg = document.getElementById("img014");
 var captionText = document.getElementById("caption4");
 img.onclick = function(){
@@ -134,7 +156,7 @@ span.onclick = function() {
 var modal = document.getElementById("myModal5");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg5");
+var img = document.getElementById("myImg6");
 var modalImg = document.getElementById("img015");
 var captionText = document.getElementById("caption5");
 img.onclick = function(){

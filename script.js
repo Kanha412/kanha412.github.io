2b9cff9cbf8f14e05794c245c970e6292a85da28
KANHA GUPTA <kanhagupta412@gmail.com>
2022-08-07 22:27:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -236,3 +236,22 @@ function openModal() {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
   }
+
+  var mybutton0 = document.getElementById("myBtn0");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton0.style.display = "block";
+  } else {
+    mybutton0.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

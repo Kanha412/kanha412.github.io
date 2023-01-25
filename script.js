03fd8d8552921ff58ed3201e4884e3e103d91eb9
KANHA GUPTA <kanhagupta412@gmail.com>
2023-01-25 18:42:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -283,6 +283,29 @@ span.onclick = function () {
 }
 
 
+// Get the modal
+var modal = document.getElementById("myModal11");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg12");
+var modalImg = document.getElementById("img021");
+var captionText = document.getElementById("caption11");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close11")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+
+
 
 function openModal() {
   document.getElementById("myModalt").style.display = "block";
